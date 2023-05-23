f8f3fd637f38d3667ee28a6f0e0f30547375495e
Luzhanskiy7 <107493221+Luzhanskiy7@users.noreply.github.com>
2023-05-23 23:22:29 +0300

    Update codeWars.js
    added task3

--- codeWars.js
+++ codeWars.js
@@ -1,3 +1,4 @@
+//_____________________________________________________________________ TASK2
 // https://www.codewars.com/kata/convert-a-string-to-an-array/train/javascript
 function stringToArray(string){
 return string.split(' ');
@@ -74,4 +75,30 @@ function firstNonConsecutive (arr) {
     }
   }
         return null;  
+}
+// ______________________________________________________________________________TASK3
+// https://www.codewars.com/kata/find-the-smallest-integer-in-the-array/train/javascript
+class SmallestIntegerFinder {
+  findSmallestInt(args) {
+    return Math.min(...args);  
+  }
+}
+
+// https://www.codewars.com/kata/geometry-basics-circle-circumference-in-2d/train/javascript
+function circleCircumference(circle) {
+    return Number((2 * Math.PI * circle.radius).toFixed(6));
+}
+
+// https://www.codewars.com/kata/training-js-number-12-loop-statement-for-dot-in-and-for-dot-of/train/javascript
+function giveMeFive(obj) {
+  let array = [];
+  for (let key in obj) {
+    if (key.length === 5) {
+      array.push(key);
+    }
+    if (typeof obj[key] === 'string' && obj[key].length === 5) {
+      array.push(obj[key]);
+    }
+  }
+    return array;
 }
